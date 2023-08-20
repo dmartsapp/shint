f5a4a3216420cbf5637685c4ac517f11d5b2986a
Farhan Sabbir Siddique <fsabbir@gmail.com>
2023-08-20 20:25:36 +0600

    tcp resolve
    - no timeout yet

--- main.go
+++ main.go
@@ -3,7 +3,10 @@ package main
 import (
 	"flag"
 	"fmt"
+	"net"
 	"os"
+	"strconv"
+	"time"
 )
 
 var (
@@ -13,7 +16,6 @@ var (
 )
 
 func init() {
-	fmt.Println("Initializing...")
 	flag.IntVar(&iterations, "n", 1, "Number of times to check")
 	flag.BoolVar(&payload, "payload", false, "Check if payload can be downloaded")
 	udp = flag.Bool("udp", false, "Use UDP instead of tcp to connect to endpoint")
@@ -24,6 +26,18 @@ func main() {
 	if len(flag.Args()) < 2 {
 		flag.PrintDefaults()
 		os.Exit(0)
+	}
+	if !*udp {
+		addr := flag.Args()[0] + ":" + flag.Args()[1]
+		tcp, err := net.ResolveTCPAddr("tcp", addr)
+		if err != nil {
+			fmt.Println(err.Error())
+			os.Exit(1)
+		}
+		start := time.Now()
+		_, err = net.DialTCP("tcp", nil, tcp)
+		end := time.Now()
+		fmt.Printf("lo %v", strconv.Itoa(int(end.Sub(start).Milliseconds())))
 	}
 
 }
