1237cf4d00dfc8392b736bbea7620b307dfa8b4c
Farhan Sabbir Siddique <fsabbir@gmail.com>
2023-08-21 10:27:20 +0600

    renamed parameters

--- main.go
+++ main.go
@@ -17,9 +17,9 @@ var (
 )
 
 func init() {
-	flag.IntVar(&iterations, "n", 1, "Number of times to check")
-	flag.IntVar(&timeout, "t", 5, "Timeout in seconds to connect")
-	flag.BoolVar(&payload, "payload", false, "Check if payload can be downloaded")
+	flag.IntVar(&iterations, "iterations", 1, "Number of times to check")
+	flag.IntVar(&timeout, "timeout", 5, "Timeout in seconds to connect")
+	flag.BoolVar(&payload, "download", false, "Check if payload can be downloaded")
 	udp = flag.Bool("udp", false, "Use UDP instead of tcp to connect to endpoint")
 }
 
