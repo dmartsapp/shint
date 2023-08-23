bd26272741df2812b1a3459f40ba8dc01b4a642e
Farhan Sabbir Siddique <fsabbir@gmail.com>
2023-08-23 22:28:24 +0600

    handling tcp-web first
    - formats url from provided options and flags
    - adds IP resolver method to resolve names when meets regex check

--- main.go
+++ main.go
@@ -5,23 +5,29 @@ import (
 	"fmt"
 	"net"
 	"os"
+	"regexp"
+	"strconv"
 	"time"
 )
 
 var (
-	payload    bool
+	download   *bool
 	iterations int
 	udp        *bool
 	timeout    int
-	scheme     string
+	httpOnly   *bool
+	web        *bool
+	path       string
 )
 
 func init() {
 	flag.IntVar(&iterations, "iterations", 1, "Number of times to check")
 	flag.IntVar(&timeout, "timeout", 5, "Timeout in seconds to connect")
-	flag.BoolVar(&payload, "download", false, "Check if payload can be downloaded")
-	flag.StringVar(&scheme, "scheme", "https", "URL Scheme to use for web requests (http/https are only supported for now)")
-	udp = flag.Bool("udp", false, "Use UDP instead of tcp to connect to endpoint")
+	udp = flag.Bool("udp", false, "Flag option (Doesn't expect any value after option). Use UDP instead of tcp to connect to endpoint")
+	flag.StringVar(&path, "path", "/", "Path to send web request to. Requires 'web' flag set first.")
+	download = flag.Bool("download", false, "Flag option (Doesn't expect any value after option). Download the contents of web request and print to STDOUT. Requires 'web' flag.")
+	httpOnly = flag.Bool("http", false, "Flag option (Doesn't expect any value after option). Use http instead of default https for web requests. Requires 'web' flag.")
+	web = flag.Bool("web", false, "Flag option (Doesn't expect any value after option). Use web request on top of regular telnet. 'http' and 'download' flags and 'path' option only works if this flag is used.")
 
 	flag.Usage = func() {
 		fmt.Println("Usage: " + os.Args[0] + " [options] <fqdn|IP> port")
@@ -30,12 +36,12 @@ func init() {
 		fmt.Println()
 		fmt.Println("Example (fqdn): " + os.Args[0] + " google.com 443")
 		fmt.Println("Example (IP): " + os.Args[0] + " 10.10.10.10 443")
-		fmt.Println("Example (fqdn with scheme): " + os.Args[0] + "-scheme https 10.10.10.10 443")
+		fmt.Println("Example (fqdn with -web and -http flags to send 'http' request to path '/pages/index.html' as 'web' client): " + os.Args[0] + " -web -http -path '/pages/index.html' 10.10.10.10 443")
 		os.Exit(0)
 	}
 }
 
-func flagExists(flagname string) bool {
+func optionExists(flagname string) bool {
 	found := false
 	flag.Visit(func(f *flag.Flag) {
 		if f.Name == flagname {
@@ -46,6 +52,15 @@ func flagExists(flagname string) bool {
 	return found
 }
 
+func resolveName(ipaddress string) *net.IPAddr {
+	ip, err := net.ResolveIPAddr("", ipaddress)
+	if err != nil {
+		fmt.Println(err.Error())
+		os.Exit(1)
+	}
+	return ip
+}
+
 func main() {
 
 	flag.Parse()
@@ -53,10 +68,44 @@ func main() {
 		flag.Usage()
 	}
 
+	regex, _ := regexp.Compile("[a-z|A-Z]")
+
 	if !*udp {
-		if flagExists("scheme") {
-			fmt.Println("Exists")
+		ip := flag.Args()[0]
+		if regex.MatchString(flag.Args()[0]) {
+			start := time.Now()
+			ip = resolveName(flag.Args()[0]).String()
+			end := time.Now()
+			fmt.Println("Resolved '" + flag.Args()[0] + "' to '" + ip + "' in " + strconv.Itoa(int(end.Sub(start).Milliseconds())) + "ms")
+
 		}
+
+		if *web {
+			// this is web request; Check for other flags
+			scheme := "https"
+			getpath := "/"
+
+			if *httpOnly {
+				scheme = "http"
+			}
+			if optionExists("path") {
+				getpath = path
+			}
+
+			url := scheme + "://" + flag.Args()[0] + ":" + flag.Args()[1] + getpath
+			fmt.Println(url)
+			if *download {
+				fmt.Println("Placeholder for web request download")
+				// this is for downloading entire payload; No summary
+			}
+
+		} else {
+			// this is regular TCP telnet
+			fmt.Println("Placeholder for regular TCP")
+		}
+	} else {
+		// this is for UDP request
+		fmt.Println("Placeholder for regular UDP")
 	}
 
 	// if len(flag.Args()) == 1 {
