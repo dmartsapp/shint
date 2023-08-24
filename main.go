b6e61d60da4ca7b6807fac31d79c7bd699567676
Farhan Sabbir Siddique <fsabbir@gmail.com>
2023-08-24 14:04:12 +0600

    removed unnecessary comments

--- main.go
+++ main.go
@@ -129,60 +129,6 @@ func main() {
 		// this is for UDP request
 		fmt.Println("Placeholder for regular UDP")
 	}
-
-	// if len(flag.Args()) == 1 {
-	// 	url, err := url.Parse(flag.Args()[0])
-	// 	if err != nil {
-	// 		fmt.Println(err.Error())
-	// 		os.Exit(1)
-	// 	}
-
-	// 	port, _ := strconv.Atoi(url.Port())
-	// 	if port == 0 {
-	// 		switch url.Scheme {
-	// 		case "http":
-	// 			port = 80
-	// 		case "https":
-	// 			port = 443
-	// 		default:
-	// 			port = 443
-	// 		}
-	// 	}
-
-	// 	resp, _ := http.Get(url.String())
-
-	// 	fmt.Println(resp.StatusCode)
-
-	// 	/*
-	// 		placeholder for http or https check with or without payload download
-	// 	*/
-	// } else {
-	// 	if !*udp {
-	// 		addr := flag.Args()[0] + ":" + flag.Args()[1]
-	// 		start := time.Now()
-	// 		tcpaddr, err := net.ResolveTCPAddr("tcp", addr)
-	// 		end := time.Now()
-	// 		if err != nil {
-	// 			fmt.Println(err.Error())
-	// 			os.Exit(1)
-	// 		}
-	// 		fmt.Println("Resolved IP:Port of '" + addr + "' to " + tcpaddr.String() + " in " + strconv.Itoa(int(end.Sub(start).Milliseconds())) + "ms")
-	// 		// var wg sync.WaitGroup
-	// 		for i := 0; i < iterations; i++ {
-	// 			// resp := dialNow("tcp", addr, timeout, &wg)
-	// 			resp := dialNow("tcp", addr, timeout)
-	// 			fmt.Println("TCP port checked successfully for '" + addr + "' in: " + strconv.Itoa(resp) + "ms")
-	// 		}
-	// 		// wg.Wait()
-
-	// 	} else {
-	// 		/*
-	// 			Placeholder for UDP connection
-	// 		*/
-	// 		fmt.Println("UDP")
-	// 	}
-	// }
-
 }
 
 // func dialNow(protocol string, addressport string, timeout int, wg *sync.WaitGroup) int {
