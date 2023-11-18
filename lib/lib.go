32499e12c14b9fe4315bd25631871c1ec3ca8d5a
Farhan Sabbir Siddique <fsabbir@gmail.com>
2023-11-19 00:41:31 +0600

    telnet done so far
    - to do stats comparator
    - possible go-routine for telnet clients?

--- lib/lib.go
+++ lib/lib.go
@@ -1,10 +1,27 @@
 package lib
 
 import (
+	"context"
 	"net"
+	"time"
 )
 
-func ResolveName(name string) ([]net.IP, error) {
-	ipaddresses, err := net.LookupIP(name)
+const (
+	DATETIMEFORMAT string = "Mon Jan 2 15:04:05 MST 2006"
+	NetworkType    string = "ip4" // other networks are ip which includes both v4 and v6, and ip6 which is only v6
+	Protocol       string = "tcp"
+)
+
+func ResolveName(ctx context.Context, name string) ([]net.IP, error) {
+	var resolver net.Resolver
+	ipaddresses, err := resolver.LookupIP(ctx, NetworkType, name)
 	return ipaddresses, err
+}
+
+func LogWithTimestamp(log string, iserror bool) string {
+	if !iserror {
+		return time.Now().Format(DATETIMEFORMAT) + ": " + log
+	}
+	return time.Now().Format(DATETIMEFORMAT) + ": Error! " + log
+
 }
--- main.go
+++ main.go
@@ -1,20 +1,27 @@
 package main
 
 import (
+	"context"
 	"flag"
 	"fmt"
+	"net"
 	"os"
+	"strconv"
+	"strings"
+	"time"
+
+	"github.com/farhansabbir/telnet/lib"
 )
 
 var (
-	iterations int
+	iterations int = 1
 	delay      int
 	throttle   *bool
-	timeout    int
+	timeout    int = 5
 	web        *bool
 	nmap       *bool
-	fromport   int
-	endport    int
+	fromport   int = 1
+	endport    int = 80
 )
 
 const (
@@ -23,14 +30,14 @@ const (
 )
 
 func init() {
-	flag.IntVar(&iterations, "count", 1, "Number of times to check connectivity")
-	flag.IntVar(&timeout, "timeout", 5, "Timeout in seconds to connect")
-	flag.IntVar(&delay, "delay", 0, "Seconds delay between each iteration given in count")
+	flag.IntVar(&iterations, "count", iterations, "Number of times to check connectivity")
+	flag.IntVar(&timeout, "timeout", timeout, "Timeout in seconds to connect")
+	flag.IntVar(&delay, "delay", delay, "Seconds delay between each iteration given in count")
 	web = flag.Bool("web", false, "Use web request as a web client.")
-	throttle = flag.Bool("throttle", false, "Flag option to throttle between every iteration of count to simulate non-uniform request.")
+	throttle = flag.Bool("throttle", false, "Flag option to throttle between every iteration of count to simulate non-uniform request. This is useful for networks/systems with AV or IDS")
 	nmap = flag.Bool("nmap", false, "Flag option to run tcp port scan. This flag ignores all other parameters except -from and -to, if mentioned.")
-	flag.IntVar(&fromport, "from", 1, "Start port to begin TCP scan from.")
-	flag.IntVar(&endport, "to", 80, "End port to run TCP scan to.")
+	flag.IntVar(&fromport, "from", fromport, "Start port to begin TCP scan from.")
+	flag.IntVar(&endport, "to", endport, "End port to run TCP scan to.")
 
 	flag.Usage = func() {
 		fmt.Println("Usage: " + os.Args[0] + " [options] <fqdn|IP> port")
@@ -45,13 +52,59 @@ func init() {
 }
 
 func main() {
-
 	flag.Parse()         // read the flags passed for processing
 	if !*web || !*nmap { // nmap and web needs single param like -nmap 10.10.18.121 or "-web https://google.com" respectively, while telnet needs two parameters like 10.10.18.121 22 for IP and Port respectively
 		if len(flag.Args()) != 2 { // telnet only needs 2 params, so show usage and exit for additional parameters
 			flag.Usage()
 			os.Exit(int(SuccessNoError))
 		}
+	}
+	// setting up timeout context to ensure we exit after defined timeout
+	CTXTIMEOUT, CANCEL := context.WithTimeout(context.Background(), time.Duration(time.Second*time.Duration(timeout)))
+	defer CANCEL()
+
+	if *nmap {
+
+	} else if *web {
+
+	} else { // this should be ideally telnet if not web or nmap
+		port, err := strconv.ParseUint(flag.Arg(1), 10, 64)
+		if err != nil {
+			fmt.Println(lib.LogWithTimestamp(err.Error(), true))
+			os.Exit(1)
+		}
+		start := time.Now()
+		ipaddresses, err := lib.ResolveName(CTXTIMEOUT, flag.Arg(0))
+		fmt.Println(lib.LogWithTimestamp("DNS lookup successful in "+time.Since(start).String(), false))
+		if err != nil {
+			fmt.Printf("%s ", lib.LogWithTimestamp(err.Error(), true))
+		} else {
+			var stats = make([]time.Duration, iterations)
+
+			for i := 0; i < iterations; i++ { // loop over the ip addresses for the iterations required
+				for _, ip := range ipaddresses { //  we need to loop over all ip addresses returned, even for once
+					var dialer net.Dialer
+					start = time.Now()
+					conn, err := dialer.DialContext(CTXTIMEOUT, lib.Protocol, ip.String()+":"+strconv.Itoa(int(port)))
+					if err != nil {
+						if strings.Contains(err.Error(), "i/o timeout") {
+							fmt.Println(lib.LogWithTimestamp("Timeout connecting to "+ip.String()+" on port "+strconv.Itoa(int(port))+" after "+time.Since(start).String(), true))
+						} else {
+							fmt.Println(lib.LogWithTimestamp(err.Error(), true))
+						}
+						stats = append(stats, time.Since(start))
+						continue
+					} else {
+						stats = append(stats, time.Since(start))
+						fmt.Println(lib.LogWithTimestamp("Successfully connected to "+ip.String()+" on port "+strconv.Itoa(int(port))+" after "+time.Since(start).String(), false))
+					}
+					conn.Close()
+				}
+			}
+			fmt.Println(stats)
+
+		}
+
 	}
 
 }
