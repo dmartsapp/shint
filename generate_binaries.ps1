82826f4643bbe6b219aa32108fbe9ede073fd228
Farhan Sabbir Siddique <fsabbir@gmail.com>
2025-02-17 08:49:24 +0600

    updated powershell binary generator to reflect the same binary size decrease process by CGO disable

--- generate_binaries.ps1
+++ generate_binaries.ps1
@@ -1,5 +1,5 @@
 foreach ($os in @("windows","linux","darwin")){
     foreach($arch in @("arm64","amd64")){
-        $Env:GOOS="$os"; $Env:GOARCH="$arch"; go build -buildvcs=true -o bin/telnet.$os.$arch
+        $Env:GOOS="$os"; $Env:GOARCH="$arch"; $Env:CGO_ENABLED=0; go build -buildvcs=true -ldflags="-s -w" -o bin/telnet.$os.$arch
     }
 }
