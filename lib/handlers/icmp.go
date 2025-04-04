9c862631e077cd437958c317a7ad5945dd653d55
Farhan Sabbir Siddique <fsabbir@gmail.com>
2025-04-04 08:28:55 -0400

    moved out icmp from main

---
+++ lib/handlers/icmp.go
@@ -0,0 +1,95 @@
+package handlers
+
+import (
+	"encoding/json"
+	"fmt"
+	"os"
+	"sync"
+	"time"
+
+	"github.com/farhansabbir/go-ping/netutils"
+	"github.com/farhansabbir/telnet/lib"
+)
+
+func HandleICMP(host string, jsonoutput *bool, iterations int, delay int, throttle *bool, timeout int, payload_size int) {
+
+	output := lib.JSONOutput{}
+	output.InputParams = lib.InputParams{
+		Mode:     "icmp",
+		Host:     host,
+		FromPort: int(7),
+		ToPort:   int(7),
+		Protocol: "icmp",
+		Timeout:  timeout,
+		Count:    iterations,
+		Delay:    delay,
+		Payload:  payload_size,
+		Throttle: *throttle,
+	}
+	output.ModuleName = "icmp"
+	start := time.Now()
+	pinger, err := netutils.NewPinger(host)
+	if err != nil {
+		fmt.Println(lib.LogWithTimestamp(err.Error(), true))
+		os.Exit(1)
+	}
+
+	wg := sync.WaitGroup{}
+
+	if !*jsonoutput {
+		wg.Add(1)
+		go func(pinger *netutils.Pinger, wg *sync.WaitGroup) {
+			defer wg.Done()
+			for log := range pinger.StreamLog() {
+				fmt.Println(lib.LogWithTimestamp(log, false))
+			}
+		}(pinger, &wg)
+	}
+
+	pinger.
+		SetPingCount(iterations).
+		SetParallelPing(true).
+		SetPayloadSizeInBytes(payload_size).
+		SetPingDelayInMS(delay).
+		SetRandomizedPingDelay(*throttle)
+	err = pinger.PingAll()
+	if err != nil {
+		fmt.Println(lib.LogWithTimestamp(err.Error(), true))
+		os.Exit(1)
+	}
+
+	wg.Wait()
+	pinger.MeasureStats()
+	// pinger.MeasureStats()
+	if !*jsonoutput {
+		fmt.Println("========================================= Ping stats ============================================")
+		fmt.Printf("Packets sent: %d, Packets received: %d, Packets lost: %d, Ping success: %d%% \n", pinger.Count*len(pinger.Destination), (pinger.Count*len(pinger.Destination) - pinger.Stats.Loss), pinger.Stats.Loss, ((pinger.Count*len(pinger.Destination) - pinger.Stats.Loss) * 100 / (pinger.Count * len(pinger.Destination))))
+		fmt.Printf("Total time: %v, Resolve time: %v\n", pinger.Stats.TotalTime, pinger.Stats.ResolveTime)
+		fmt.Printf("Min time: %dms, Max time: %dms, Avg time: %.3fms, Std dev: %.3f, Total time: %v\n", pinger.Stats.Min, pinger.Stats.Max, pinger.Stats.Avg, pinger.Stats.StdDev, pinger.Stats.TotalTime)
+	} else {
+		output.DNSLookup = lib.DNSLookup{
+			Hostname:          host,
+			Success:           true,
+			ResolvedAddresses: lib.ConvertIPToStringSlice(pinger.Destination),
+			TimeTaken:         pinger.Stats.ResolveTime.Microseconds(),
+		}
+		output.StartTime = start.UnixMicro()
+		output.EndTime = time.Now().UnixMicro()
+		output.TotalTimeTaken = output.EndTime - output.StartTime
+		output.Stats = make([]lib.ICMPStats, 0)
+		for _, pckts := range pinger.Stats.Packets {
+			stat := lib.ICMPStats{}
+			stat.Address = pckts.Destination.String()
+			stat.Success = !pckts.ErrorEncountered
+			stat.Sequence = pckts.Sequence
+			stat.SentTime = pckts.SentDateTimeUNIX
+			stat.RecvTime = pckts.ReceiveDateTimeUNIX
+			stat.TimeTaken = stat.RecvTime - stat.SentTime
+			output.Stats = append(output.Stats.([]lib.ICMPStats), stat)
+		}
+
+		JS, _ := json.MarshalIndent(output, "", "  ")
+		fmt.Println(string(JS))
+
+	}
+}
--- main.go
+++ main.go
@@ -18,7 +18,6 @@ import (
 	"sync"
 	"time"
 
-	"github.com/farhansabbir/go-ping/netutils"
 	"github.com/farhansabbir/telnet/lib"
 	"github.com/farhansabbir/telnet/lib/handlers"
 )
@@ -272,85 +271,7 @@ func main() {
 			fmt.Println(lib.LogWithTimestamp("Missing URL/address to ping", true))
 			os.Exit(1)
 		}
-		output := lib.JSONOutput{}
-		output.InputParams = lib.InputParams{
-			Mode:     "icmp",
-			Host:     flag.Arg(0),
-			FromPort: int(7),
-			ToPort:   int(7),
-			Protocol: "icmp",
-			Timeout:  timeout,
-			Count:    iterations,
-			Delay:    delay,
-			Payload:  payload_size,
-			Throttle: *throttle,
-		}
-		output.ModuleName = "icmp"
-		start := time.Now()
-		pinger, err := netutils.NewPinger(flag.Arg(0))
-		if err != nil {
-			fmt.Println(lib.LogWithTimestamp(err.Error(), true))
-			os.Exit(1)
-		}
-
-		wg := sync.WaitGroup{}
-
-		if !*jsonoutput {
-			wg.Add(1)
-			go func(pinger *netutils.Pinger, wg *sync.WaitGroup) {
-				defer wg.Done()
-				for log := range pinger.StreamLog() {
-					fmt.Println(lib.LogWithTimestamp(log, false))
-				}
-			}(pinger, &wg)
-		}
-
-		pinger.
-			SetPingCount(iterations).
-			SetParallelPing(true).
-			SetPayloadSizeInBytes(payload_size).
-			SetPingDelayInMS(delay).
-			SetRandomizedPingDelay(*throttle)
-		err = pinger.PingAll()
-		if err != nil {
-			fmt.Println(lib.LogWithTimestamp(err.Error(), true))
-			os.Exit(1)
-		}
-
-		wg.Wait()
-		pinger.MeasureStats()
-		// pinger.MeasureStats()
-		if !*jsonoutput {
-			fmt.Println("========================================= Ping stats ============================================")
-			fmt.Printf("Packets sent: %d, Packets received: %d, Packets lost: %d, Ping success: %d%% \n", pinger.Count*len(pinger.Destination), (pinger.Count*len(pinger.Destination) - pinger.Stats.Loss), pinger.Stats.Loss, ((pinger.Count*len(pinger.Destination) - pinger.Stats.Loss) * 100 / (pinger.Count * len(pinger.Destination))))
-			fmt.Printf("Total time: %v, Resolve time: %v\n", pinger.Stats.TotalTime, pinger.Stats.ResolveTime)
-			fmt.Printf("Min time: %dms, Max time: %dms, Avg time: %.3fms, Std dev: %.3f, Total time: %v\n", pinger.Stats.Min, pinger.Stats.Max, pinger.Stats.Avg, pinger.Stats.StdDev, pinger.Stats.TotalTime)
-		} else {
-			output.DNSLookup = lib.DNSLookup{
-				Hostname:          flag.Arg(0),
-				Success:           true,
-				ResolvedAddresses: lib.ConvertIPToStringSlice(pinger.Destination),
-				TimeTaken:         pinger.Stats.ResolveTime.Microseconds(),
-			}
-			output.StartTime = start.UnixMicro()
-			output.EndTime = time.Now().UnixMicro()
-			output.TotalTimeTaken = output.EndTime - output.StartTime
-			output.Stats = make([]lib.ICMPStats, 0)
-			for _, pckts := range pinger.Stats.Packets {
-				stat := lib.ICMPStats{}
-				stat.Address = pckts.Destination.String()
-				stat.Success = !pckts.ErrorEncountered
-				stat.Sequence = pckts.Sequence
-				stat.SentTime = pckts.SentDateTimeUNIX
-				stat.RecvTime = pckts.ReceiveDateTimeUNIX
-				stat.TimeTaken = stat.RecvTime - stat.SentTime
-				output.Stats = append(output.Stats.([]lib.ICMPStats), stat)
-			}
-
-			JS, _ := json.MarshalIndent(output, "", "  ")
-			fmt.Println(string(JS))
-
-		}
+		handlers.HandleICMP(flag.Arg(0), jsonoutput, iterations, delay, throttle, timeout, payload_size)
 
 	} else { // this should be ideally telnet if not web or nmap or ping
 		port, err := strconv.ParseUint(flag.Arg(1), 10, 64)
