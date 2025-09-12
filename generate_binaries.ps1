c3807b7f8a06504ce49cf86325e83fae8e7c12ce
Farhan Sabbir Siddique <fsabbir@gmail.com>
2025-09-12 02:19:09 -0600

    patch release
    - removed makefiles and binary generators
    - release CI complete
    - versioning from app reflects SHA and version number

--- generate_binaries.ps1
+++
@@ -1,5 +0,0 @@
-foreach ($os in @("windows","linux","darwin")){
-    foreach($arch in @("arm64","amd64")){
-        $Env:GOOS="$os"; $Env:GOARCH="$arch"; $Env:CGO_ENABLED=0; go build -buildvcs=true -ldflags="-s -w" -o bin/telnet.$os.$arch
-    }
-}
